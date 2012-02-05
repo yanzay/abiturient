30298b3e54675e612449c1d02659184f30645b14
Yanzay <yanzay@mail.ru>
2012-02-05 22:34:38 +0200

    many edits

--- abit/forms.py
+++ abit/forms.py
@@ -1,14 +1,14 @@
+# coding=utf8
+
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
 from django.forms.widgets import DateInput
 from abiturient.abit.models import AbitRequest
 
 class AbitRequestForm(forms.ModelForm):
-    birth_date = forms.DateField(widget=DateInput(attrs={"class":"date"}))
+    birth_date = forms.DateField(label='Дата рождения', widget=DateInput(attrs={"class":"date"}))
+    passport_date = forms.DateField(label='Дата выдачи',widget=DateInput(attrs={"class":"date"}))
+    att_date = forms.DateField(label='Дата выдачи аттестата',widget=DateInput(attrs={"class":"date"}))
+
     class Meta:
         model = AbitRequest
-
-#class TestResultForm(forms.ModelForm):
-#    class Meta:
-#        model = TestResult
-#        exclude = ('abit', 'request')
