9a107df4fa98f21442ec17216c93adefc00c3174
Yanzay <yanzay@mail.ru>
2012-02-29 12:16:01 +0200

    disable jenkins

--- settings.py
+++ settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'abiturient.abit',
-    'django_jenkins',
+#    'django_jenkins',
 )
 
 # A sample logging configuration. The only tangible logging
